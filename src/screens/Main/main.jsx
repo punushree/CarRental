0e5220c631b4bbfa2b587ec3dbf48ca6ba1c1d25
sabiha-bugendaitech <sabiha.anjum@bugendaitech.com>
2022-09-03 12:19:22 +0530

    Adding screens

---
+++ src/screens/Main/main.jsx
@@ -0,0 +1,31 @@
+import React from "react"
+import JsonData from "../../data/data.json";
+import { useState, useEffect } from "react";
+import Home from "../home/home.jsx";
+import About from "../About/about.jsx"
+import Services from "../services/services.js"
+import Contact from "../contact/contact.js"
+import Car from "../Car/car.jsx";
+import Pricing from "../Pricing/pricing.jsx"
+const Main = (props) => {
+    const [landingPageData, setLandingPageData] = useState({});
+    useEffect(() => {
+      setLandingPageData(JsonData);
+    }, []);
+  return (
+    <>
+<Home data={landingPageData.Header} />
+       <Car />
+
+       <Services data={landingPageData.Services} />
+
+       <About data={landingPageData.About} />
+       <Pricing data="" />
+
+       <Contact data={landingPageData.Contact} />
+
+    </>
+  )
+}
+
+export default Main
